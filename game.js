e07a5a0c496486fd745c07083fef14052bc6043e
phanatuan <phananhtuan1011@gmail.com>
2019-03-08 11:38:37 +0700

    Fix/fix render (#4)
    * Fixing Rendering, still got bugs
    
    * Fixing bugs, 50 percent through the fixing render, still a lot of mess though
    
    * Fix more bugs on the logic of endgame. Need to work on isRestart variable
    
    * Fix Hero Boundary. Gameover still buggy

--- game.js
+++ game.js
@@ -1,57 +1,44 @@
-let canvas;
-let ctx;
-let timeStart;
-let timeEnd;
-let totalTime;
+let canvas, ctx;
+let timeStart, timeEnd, totalTime;
+let timeAllow = 20;
+let isRestart = false;
+let bgReady, heroReady, monsterReady;
+let bgImage, heroImage, monsterImage;
+
+let monsterX = 100;
+let monsterY = 100;
+let countMonsterCaught = 0;
+let keysDown = {};
 
 canvas = document.createElement("canvas");
 ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
+canvas.width = 500;
+canvas.height = 500;
 document.body.appendChild(canvas);
 
-let bgReady, heroReady, monsterReady;
-let bgImage, heroImage, monsterImage;
+let heroX = canvas.width / 2;
+let heroY = canvas.height / 2;
 
 function loadImages() {
   bgImage = new Image();
   bgImage.onload = function() {
-    // show the background image
     bgReady = true;
   };
   bgImage.src = "images/background.png";
 
   heroImage = new Image();
   heroImage.onload = function() {
-    // show the hero image
     heroReady = true;
   };
   heroImage.src = "images/hero.png";
 
   monsterImage = new Image();
   monsterImage.onload = function() {
-    // show the monster image
     monsterReady = true;
   };
   monsterImage.src = "images/monster.png";
 }
 
-let heroX = canvas.width / 2;
-let heroY = canvas.height / 2;
-
-generateLocation = canvas => {
-  let x = Math.random() * canvas.width + 1;
-  let y = Math.random() * canvas.height + 1;
-  return { x, y };
-};
-
-let monsterX = 100;
-let monsterY = 100;
-let countMonsterCaught = 0;
-
-// Keyboard Listeners
-
-let keysDown = {};
 function setupKeyboardListeners() {
   addEventListener(
     "keydown",
@@ -74,6 +61,12 @@ function setupKeyboardListeners() {
   );
 }
 
+const generateLocation = canvas => {
+  let x = Math.random() * canvas.width + 1;
+  let y = Math.random() * canvas.height + 1;
+  return { x, y };
+};
+
 const update = () => {
   if (38 in keysDown) {
     //UP
@@ -92,37 +85,55 @@ const update = () => {
     heroX += 2;
   }
 
-  // Check if player and monster collided
+  let { characterX, characterY } = notExceedCanvasBoundary(heroX, heroY);
+  heroX = characterX;
+  heroY = characterY;
+
   if (
     heroX <= monsterX + 32 &&
     monsterX <= heroX + 32 &&
     heroY <= monsterY + 32 &&
     monsterY <= heroY + 32
   ) {
-    // Pick a new location for the monster.
-    if (countMonsterCaught >= 3) {
-      timeEnd = performance.now();
-      totalTime = timeEnd - timeStart;
-      console.log(`Total Time ${totalTime}`);
-      gameOver();
-    } else {
-      countMonsterCaught++;
-      let { x, y } = generateLocation(canvas); // destructuring
-      monsterX = x; //generate new location for Monster
-      monsterY = y;
-    }
-    if (totalTime) {
-      document.getElementById("timer").innerHTML = `${(totalTime/1000).toFixed(2)} seconds`;
-    }
-    document.getElementById("score").innerHTML = countMonsterCaught;
+    countMonsterCaught++;
+    let { x, y } = generateLocation(canvas); // destructuring
+    monsterX = x; //generate new location for Monster
+    monsterY = y;
+  }
+
+  if (countMonsterCaught > 3) {
+    gameOver();
   }
 };
 
-const gameOver = () => {
+const notExceedCanvasBoundary = (characterX, characterY) => {
+  characterX = Math.min(characterX, canvas.width - 40);
+  characterX = Math.max(characterX, 0);
+  characterY = Math.min(characterY, canvas.height - 40);
+  characterY = Math.max(characterY, 0);
+  return { characterX, characterY };
+};
+
+const resetGlobalVariable = () => {
   heroX = canvas.width / 2;
   heroY = canvas.height / 2;
-  startGame();
-  document.getElementById("score").innerHTML = countMonsterCaught;
+  let { x, y } = generateLocation(canvas); // destructuring
+  monsterX = x; //generate new location for Monster
+  monsterY = y;
+};
+
+const gameOver = () => {
+  //Reset Location of Hero
+  resetGlobalVariable();
+  ctx.textAlign = "center";
+  ctx.fillText(
+    `You Win. Congratulations. Restart (Y/N)`,
+    canvas.width / 2,
+    canvas.height / 2
+  );
+  if (32 in keysDown) {
+    console.log("Push Y");
+  }
   countMonsterCaught = 0;
 };
 
@@ -136,20 +147,32 @@ const render = function() {
   if (monsterReady) {
     ctx.drawImage(monsterImage, monsterX, monsterY);
   }
+
+  ctx.font = "15px Arial";
+  ctx.fillText(`heroX: ${heroX} - heroY: ${heroY}`, 50, 100);
+  ctx.fillText(`monsterX: ${monsterX} - monsterY: ${monsterY}`, 100, 200);
+
+  //Display Count of Monster Caught
+  ctx.font = "30px Arial";
+  ctx.fillStyle = "red";
+  ctx.fillText(`Score: ${countMonsterCaught}`, 20, 40);
+
+  //Display Timer
+  ctx.fillText(`Timer: ${timeAllow}`, 360, 40);
 };
 
-/**
- * The main game loop.
- * update + render */
-
+//The main game loop.
+//update + render
 let main = function() {
   update();
   render();
-  requestAnimationFrame(main); // Request to do this again ASAP.
+  if (countMonsterCaught > 2) {
+    gameOver();
+  } else {
+    requestAnimationFrame(main); // Request to do this again ASAP.
+  }
 };
 
-// Cross-browser support for requestAnimationFrame.
-// Safely ignore this line. It's mostly here for people with old web browsers.
 var w = window;
 requestAnimationFrame =
   w.requestAnimationFrame ||
@@ -158,10 +181,10 @@ requestAnimationFrame =
   w.mozRequestAnimationFrame;
 
 // Let's play this game!
-const startGame = () => {
+gameStart = () => {
   loadImages();
   setupKeyboardListeners();
   main();
 };
 
-startGame();
+gameStart();
